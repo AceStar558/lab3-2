788a35a750d1b7d12f10ed7d684ad97aaa8cc1d3
AceStar558 <48096544+AceStar558@users.noreply.github.com>
2019-05-12 11:35:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
+using namespace std;
 int main()
 {
-	string d = "deb http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb http://security.debian.org/ stretch/updates main contrib non-free\ndeb-src http://security.debian.org/ stretch/updates main contrib non-free\ndeb http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb http://mirror.mephi.ru/debian stretch-backports main contrib non-free\ndeb-src http://mirror.mephi.ru/debian stretch-backports main contrib non-free";
-string d1;
-	string x, y;
-	cout << "Введите слово которое хотите заменять\n";
-	cin >> x;
-	cout << "Введите слово на которое хотите заменять\n";
-	cin >> y;
-	for (int i = 0; i < d.length(); i++) {
-		if (d.substr(i, x.length()) == x)
-			d.replace(i, x.length(), y);
-		if (d[i] == 'm' && d[i + 1] == 'e' && d[i + 2] == 'p' && d[i + 3] == 'h' && d[i + 4] == 'i') {
-			d1 = d1 + "yandex";
-			i = i + 4;
-			d.replace(i, i + 4, "yandex");
-		} else
-			d1 = d1+d[i];
+	string text = "deb http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb http://security.debian.org/ stretch/updates main contrib non-free\ndeb-src http://security.debian.org/ stretch/updates main contrib non-free\ndeb http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb http://mirror.mephi.ru/debian stretch-backports main contrib non-free\ndeb-src http://mirror.mephi.ru/debian stretch-backports main contrib non-free";
+	string str1 = "yandex",
+	       str2 = "mephi";
+
+	size_t res = text.find(str2, 0);
+	if (res != string::npos) {
+		while (res != string::npos) {
+			text.replace(res, str2.size(), str1);
+			res = text.find(str2, res + str1.size());
+		}
+		cout << text << endl;
 	}
-	cout << d << endl;
 	return 0;
 }
